36a819686dbdd5280c8e7ee21c5c0abe9111d55d
Sivaramavarapu <110712120+Sivaramavarapu@users.noreply.github.com>
2022-08-24 19:35:18 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -6,10 +6,7 @@ public static void main(String args[])
   int b=20;
   int c=a+b;
 System.out.println("Hello World");
-<<<<<<< HEAD
 System.out.println("Hello Jenkins integration");
-=======
 System.out.println("Sum="+c);
 }
->>>>>>> f59cfda46be7d3d66709721dfcb42b36f3d01b27
 }
